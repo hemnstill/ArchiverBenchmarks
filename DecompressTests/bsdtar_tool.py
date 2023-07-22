affe9b679edc0744c3eeb457c7b70f1cb7d7f37b
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-07-22 10:03:50 +0400

    common_paths.py

--- DecompressTests/bsdtar_tool.py
+++ DecompressTests/bsdtar_tool.py
@@ -2,16 +2,15 @@ import os
 import subprocess
 import sys
 
-_self_path: str = os.path.dirname(os.path.realpath(__file__))
-tools_path = os.path.join(os.path.dirname(_self_path), '.tools')
+from DecompressTests import common_paths
 
 
 def get_bsdtar_exe_path():
     if sys.platform.startswith('win'):
-        return os.path.join(tools_path, 'bsdtar-3.6.2.exe')
+        return os.path.join(common_paths.tools_path, 'bsdtar-3.6.2.exe')
 
-    return os.path.join(tools_path, 'bsdtar-3.6.2')
+    return os.path.join(common_paths.tools_path, 'bsdtar-3.6.2')
 
 
 def extract(file_path: str, output_dir_path: str):
-    subprocess.run([get_bsdtar_exe_path(), '-xf', file_path, '-C', output_dir_path])
+    subprocess.run([get_bsdtar_exe_path(), '-xf', file_path, '-C', output_dir_path], check=True)
--- DecompressTests/decompress_tests.py
+++ DecompressTests/decompress_tests.py
@@ -5,11 +5,7 @@ import unittest
 from timeit import timeit
 from typing import Callable
 
-from DecompressTests import wget_tool, bsdtar_tool, io_tools, p7zip_tool
-
-_self_path: str = os.path.dirname(os.path.realpath(__file__))
-data_path: str = os.path.join(_self_path, 'data')
-extracted_data_path: str = os.path.join(_self_path, 'extracted_data')
+from DecompressTests import wget_tool, bsdtar_tool, io_tools, p7zip_tool, common_paths
 
 
 @dataclasses.dataclass
@@ -34,7 +30,8 @@ class ExecutionInfo:
 def artifacts_data() -> dict[str, ArtifactInfo]:
     return {
         '7MB.7z': ArtifactInfo(name='7MB.7z', size=8023251),
-        # '116MB.zip': ArtifactInfo(name='116MB.zip', size=122518995),
+        '116MB.zip': ArtifactInfo(name='116MB.zip', size=122518995),
+        '154MB.tar.gz': ArtifactInfo(name='154MB.tar.gz', size=162315691),
     }
 
 
@@ -46,7 +43,7 @@ def archiver_tools() -> dict[str, ArchiverInfo]:
 
 
 def download_artifact(artifact: ArtifactInfo) -> None:
-    artifact_file_path = os.path.join(data_path, artifact.name)
+    artifact_file_path = os.path.join(common_paths.data_path, artifact.name)
     artifact_url = f'https://github.com/hemnstill/ArchiverBenchmarks/releases/download/init/{artifact.name}'
     if not os.path.isfile(artifact_file_path) or os.path.getsize(artifact_file_path) != artifact.size:
         wget_tool.download_url(artifact_url, artifact_file_path)
@@ -59,8 +56,8 @@ class DecompressTests(unittest.TestCase):
     @classmethod
     def setUpClass(cls) -> None:
         cls.execution_info: list[ExecutionInfo] = []
-        if not io_tools.try_create_or_clean_dir(extracted_data_path):
-            raise IOError(f'Cannot try_create_or_clean_dir: {extracted_data_path}')
+        if not io_tools.try_create_or_clean_dir(common_paths.extracted_data_path):
+            raise IOError(f'Cannot try_create_or_clean_dir: {common_paths.extracted_data_path}')
 
     @classmethod
     def tearDownClass(cls) -> None:
@@ -73,10 +70,11 @@ class DecompressTests(unittest.TestCase):
     def test_extract(self):
         for artifact in artifacts_data().values():
             for archiver in archiver_tools().values():
-                output_dir_path = os.path.join(extracted_data_path, f"{artifact.name}_{archiver.name}")
+                print(f"test_extract '{artifact.name}' with '{archiver.name}'")
+                output_dir_path = os.path.join(common_paths.extracted_data_path, f"{artifact.name}_{archiver.name}")
                 os.makedirs(output_dir_path, exist_ok=True)
 
-                execution_time = round(0.5 * timeit(lambda: archiver.extract(os.path.join(data_path, artifact.name), output_dir_path), number=2), 3)
+                execution_time = round(0.5 * timeit(lambda: archiver.extract(os.path.join(common_paths.data_path, artifact.name), output_dir_path), number=2), 3)
                 self.execution_info.append(ExecutionInfo(execution_time=execution_time,
                                                          artifact=artifact,
                                                          archiver=archiver.name))
