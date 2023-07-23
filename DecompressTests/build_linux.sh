2700ec81349d988640f68f5679d8713409ea160e
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-07-23 13:06:38 +0400

    install rapidzip

--- DecompressTests/build_linux.sh
+++ DecompressTests/build_linux.sh
@@ -7,7 +7,7 @@ self_toolset_name="build-linux"
 release_version_dirpath="$dp0/release/build"
 mkdir -p "$release_version_dirpath"
 
-python3 -m pip install pygal airium
+python3 -m pip install pygal==3.0.0 airium==0.2.5 rapidgzip==0.7.0
 python3 -m unittest "$dp0/decompress_tests.py"
 
 cd "$release_version_dirpath"
