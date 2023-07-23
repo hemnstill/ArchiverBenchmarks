acc28ef649967e88a194dae28ffdf41f70619594
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2023-07-23 18:38:43 +0400

    universal archiver and unar (#4)

---
+++ DecompressTests/archiver_tools/unar_tool.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+import sys
+
+from DecompressTests import common_paths
+
+
+def get_unar_exe_path():
+    if sys.platform.startswith('win'):
+        return os.path.join(common_paths.tools_path, 'unar-1.8.1', 'unar.exe')
+
+    return 'unar'
+
+
+def extract(file_path: str, output_dir_path: str):
+    unsupported_formats = ('.7z', '.zst')
+    if file_path.endswith(unsupported_formats):
+        raise NotImplementedError(f"unar does not support: '{file_path}'")
+
+    subprocess.run([get_unar_exe_path(), '-quiet', '-output-directory', output_dir_path, '-force-overwrite', file_path],
+                   check=True)
--- DecompressTests/decompress_tests.py
+++ DecompressTests/decompress_tests.py
@@ -35,6 +35,8 @@ def get_archiver_tools() -> dict[str, models.ArchiverInfo]:
         'pigz-2.4': models.ArchiverInfo(name='pigz-2.4', extract=archiver_tools.pigz_tool.extract),
         'rapidgzip-0.7.0': models.ArchiverInfo(name='rapidgzip-0.7.0', extract=archiver_tools.rapidgzip_tool.extract),
         'python-zstandard-0.21': models.ArchiverInfo(name='python-zstandard-0.21', extract=archiver_tools.python_ztandard.extract),
+        'archiver-3.5.1': models.ArchiverInfo(name='archiver-3.5.1', extract=archiver_tools.archiver_tool.extract),
+        'unar-1.8.1': models.ArchiverInfo(name='unar-1.8.1', extract=archiver_tools.unar_tool.extract),
     }
 
 
