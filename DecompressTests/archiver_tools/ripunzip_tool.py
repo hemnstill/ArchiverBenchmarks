08d7b14e655dae32a66c5eba32bc03ed4f70910e
Alexandr Reshetnikov <hemnstill@users.noreply.github.com>
2023-07-24 09:28:23 +0400

    ripunzip-0.4.0 (#7)
    igzip cli
    rapidgzip cli

---
+++ DecompressTests/archiver_tools/ripunzip_tool.py
@@ -0,0 +1,21 @@
+import os
+import subprocess
+import sys
+
+from DecompressTests import common_paths
+
+
+def get_ripunzip_exe_path():
+    if sys.platform.startswith('win'):
+        return os.path.join(common_paths.tools_path, 'ripunzip-0.4.0.exe')
+
+    return os.path.join(common_paths.tools_path, 'ripunzip-0.4.0')
+
+
+def extract(file_path: str, output_dir_path: str):
+    supported_formats = '.zip'
+    if not file_path.endswith(supported_formats):
+        raise NotImplementedError(f"ripunzip does not support: '{file_path}'")
+    os.makedirs(output_dir_path, exist_ok=True)
+    subprocess.run(args=f'"{get_ripunzip_exe_path()}" --output-directory "{output_dir_path}" file "{file_path}"',
+                   check=True, shell=True)
