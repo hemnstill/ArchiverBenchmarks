5e8ec2d67bf05db05f8e03321ca7e35567f5e5a1
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-04 00:08:51 +0400

    change extracted_data_path

--- DecompressTests/common_paths.py
+++ DecompressTests/common_paths.py
@@ -1,7 +1,10 @@
 import os
+import sys
 
 _self_path: str = os.path.dirname(os.path.realpath(__file__))
 data_path: str = os.path.join(_self_path, 'data')
 extracted_data_path: str = os.path.join(_self_path, 'extracted_data')
+if sys.platform.startswith('win'):
+    extracted_data_path: str = os.path.join(r'C:\.github', 'extracted_data')
 tools_path = os.path.join(os.path.dirname(_self_path), '.tools')
 render_path = os.path.join(_self_path, 'release', 'build')
