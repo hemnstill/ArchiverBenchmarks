7988c5a886e8f19e51927f015bfb3b401c746098
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-04 00:23:14 +0400

    RUNNER_TOOL_CACHE v2

--- DecompressTests/common_paths.py
+++ DecompressTests/common_paths.py
@@ -4,7 +4,8 @@ import sys
 _self_path: str = os.path.dirname(os.path.realpath(__file__))
 data_path: str = os.path.join(_self_path, 'data')
 extracted_data_path: str = os.path.join(_self_path, 'extracted_data')
-if sys.platform.startswith('win'):
-    extracted_data_path: str = os.path.join(r'C:\.github', 'extracted_data')
+if os.environ.get('RUNNER_TOOL_CACHE') and sys.platform.startswith('win'):
+    # C drive has more free space in GitHub actions on Windows.
+    extracted_data_path: str = os.path.join(os.environ['RUNNER_TOOL_CACHE'], 'extracted_data')
 tools_path = os.path.join(os.path.dirname(_self_path), '.tools')
 render_path = os.path.join(_self_path, 'release', 'build')
