417cce5b448c3a95ae5b5001224b3b99a1fe90be
Reshetnikov Alexandr <hemn.still@gmail.com>
2023-08-08 11:23:13 +0400

    file_name_encoding_tests.py

--- ArchiverCommon/common_paths.py
+++ ArchiverCommon/common_paths.py
@@ -1,4 +1,5 @@
 import os
+import pathlib
 import sys
 
 _self_path: str = os.path.dirname(os.path.realpath(__file__))
@@ -9,6 +10,14 @@ if os.environ.get('RUNNER_TOOL_CACHE') and sys.platform.startswith('win'):
     # C drive has more free space in GitHub actions on Windows.
     data_path: str = os.path.join(os.environ['RUNNER_TOOL_CACHE'], 'DecompressTests', 'data')
     extracted_data_path: str = os.path.join(os.environ['RUNNER_TOOL_CACHE'], 'DecompressTests', 'extracted_data')
+
+temp_path = os.path.join(_self_path, 'tmp')
+
+
+def create_temp_path(name: str):
+    _temp_path = os.path.join(temp_path, name)
+    pathlib.Path(_temp_path).mkdir(parents=True, exist_ok=True)
+    return _temp_path
 
 
 def create_render_path(workflow_dir_path: str):
---
+++ ArchiverCommon/tests/file_name_encoding_tests.py
@@ -0,0 +1,51 @@
+import os.path
+import pathlib
+import unittest
+
+from ArchiverCommon import io_tools, common_paths
+from ArchiverCommon.archiver_tools import bsdtar_tool, p7zip_tool
+
+
+class FileNameEncodingTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls) -> None:
+        if not io_tools.try_create_or_clean_dir(common_paths.temp_path):
+            raise IOError('try_create_or_clean_dir')
+        cls.result_dirpath = common_paths.create_temp_path('result')
+        cls.result_extracted_dirpath = common_paths.create_temp_path('result_extract')
+        cls.archive_formats = ['.zip', '.tar.gz']
+        cls.archiver_tools = [bsdtar_tool, p7zip_tool]
+        cls.utf_8_filename = 'พลัง'
+        cls.create_utf8()
+
+    @classmethod
+    def get_tool_name(cls, archiver_tool):
+        return pathlib.Path(archiver_tool.__name__).suffix
+
+    @classmethod
+    def create_utf8(cls):
+        test_archive_dirpath = common_paths.create_temp_path('test_archive')
+        utf8_file_path = os.path.join(test_archive_dirpath, cls.utf_8_filename)
+        io_tools.write_text(utf8_file_path, 'test content')
+
+        for archive_format in cls.archive_formats:
+            for archiver_tool in cls.archiver_tools:
+                archive_file_path = os.path.join(cls.result_dirpath, f'{cls.get_tool_name(archiver_tool)}{archive_format}')
+                archiver_tool.create(test_archive_dirpath, archive_file_path)
+
+    def test_extract_utf8(self):
+        for archive_format in self.archive_formats:
+            for archiver_tool in self.archiver_tools:
+                for archiver_tool_from in self.archiver_tools:
+
+                    archive_file_path = os.path.join(self.result_dirpath, f'{self.get_tool_name(archiver_tool)}{archive_format}')
+                    extracted_dir_path = os.path.join(self.result_extracted_dirpath, f"{os.path.basename(archive_file_path)}--{self.get_tool_name(archiver_tool_from)}")
+                    archiver_tool.extract(archive_file_path, extracted_dir_path)
+                    self.check_filename(extracted_dir_path)
+
+    def check_filename(self, extract_dir_path: str):
+        utf8_file_path = os.path.join(extract_dir_path, 'test_archive', self.utf_8_filename)
+        utf8_file_path2 = os.path.join(extract_dir_path, self.utf_8_filename)
+        if not pathlib.Path(utf8_file_path).is_file() and not pathlib.Path(utf8_file_path2).is_file() :
+            print(f"'{extract_dir_path}' failed.")
